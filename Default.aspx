a6d77201b6bde2455bf07ae2c454140ddeb73ef6
danny4ever12 <daniel.ephraim12@gmail.com>
2015-07-12 19:55:43 +0530

    image comparison using cloudinary, phash and nuget
    a simple website for comparison of two images and indicate how similar
    they are on a scale of 0 to 100

---
+++ Default.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<%@ Import Namespace="CloudinaryDotNet" %>
+<%@ Import Namespace="CloudinaryDotNet.Actions" %>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Image | Comparison</title>
+
+    
+</head>
+<body style="background-image: url('images/mainback.jpg'); width:100%; height:50%; margin-left: 8px;">
+
+    <form id="form1" runat="server">
+       <div>
+            <h1 style="color: #6D0770; font-family: Arial, Helvetica, sans-serif; font-size: 45px; font-style:normal; z-index: 1; left: 11px; top: 20px; height: 56px; width: 1356px; font-weight: bold;" 
+              align="center">Compare two images</h1> 
+       </div>
+       <div>
+            <p style="color: #6D0770; font-family: Calibri; font-size: xx-large; font-style: italic; z-index: 1; left: 11px; top: 20px; height: 28px; width: 400px; font-weight: bold; font-variant: normal;" 
+            align="left">Enter links of images here</p>    
+       </div>
+       <div>
+            <p style="color: #6D0770; font-family: Calibri; font-size: medium; font-style: italic; z-index: 1; left: 11px; top: 20px; height: 28px; width: 400px; font-weight: bold; font-variant: normal;" 
+            align="left">image1</p> 
+       </div>
+       <div>
+            <asp:TextBox ID="TextBox1" runat="server" BackColor="#D7D7D7" 
+            BorderColor="#9966FF" BorderStyle="Groove" ForeColor="#666666" Height="34px" 
+            Width="343px"></asp:TextBox>
+       </div>
+       <div>
+            <p style="color: #6D0770; font-family: Calibri; font-size: medium; font-style: italic; z-index: 1; left: 11px; top: 20px; height: 28px; width: 400px; font-weight: bold; font-variant: normal;" 
+            align="left">image2</p>   
+       </div>
+       <div>
+            <asp:TextBox ID="TextBox2" runat="server" BackColor="#D7D7D7" 
+            BorderColor="#9966FF" BorderStyle="Groove" ForeColor="#666666" Height="34px" 
+            Width="343px"></asp:TextBox>
+       </div>
+    
+       <p style="width: 91px; height: 4px"></p>
+   
+       <div style="width: 337px; height: 71px">
+            <asp:Button ID="Button1" runat="server" 
+             onclick="Button1_Click" Text="done" Font-Bold="True" Font-Italic="True" 
+             Font-Size="X-Large" ForeColor="White" 
+             ToolTip="click here after two links are provided" Width="189px" 
+             BackColor="#6666FF" BorderColor="#3399FF" BorderStyle="Groove" />
+       </div> 
+    
+       <div style="position:absolute; top: 185px; left: 455px; height: 136px; width: 262px;"> <asp:Image ID="Image1" runat="server"  Height="136px" Width="262px" /></div>
+       <div style="position:absolute; top: 185px; left: 740px; height: 136px; width: 262px;"> <asp:Image ID="Image2" runat="server"  Height="136px" Width="262px" /></div>
+    
+     
+       <div style="position:absolute; top: 336px; left: 455px; height: 29px; width: 547px;">
+             <p style="color: #800080; font-family: Calibri; font-size: x-large; font-style: italic; font-weight: normal; font-variant: normal;" align="left"> <%=sn1%></p> 
+       </div>
+        
+       <div style="position:absolute; top: 380px; left: 455px; height: 29px; width: 547px;">
+             <p style="color: #800080; font-family: Calibri; font-size: x-large; font-style: italic; font-weight: normal; font-variant: normal;"  align="left"> <%=sn2%></p>
+       </div>
+    </form>
+    
+  </body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+
+public partial class _Default : System.Web.UI.Page
+{
+    string phash1 = "", phash2 = "", sim_scale = "", sim_comment = "";
+
+    //returning comment and similarity scale value 
+    public string sn1 { get { return sim_comment; } }
+    public string sn2 { get { return sim_scale; } }
+  
+    //on page load
+     protected void Page_Load(object sender, EventArgs e)
+    {
+       
+
+    }
+
+    //on button click
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+       //setting cloudinary account
+        Account account = new Account(
+        "dfd9kesq3",
+        "263446946632429",
+        "3CK5mZyoZMbfhigPIKNO4LLVg2c");
+       
+        try
+        {
+            //for displaying the image provided by user
+            Image1.ImageUrl = TextBox1.Text;
+            Image2.ImageUrl = TextBox2.Text;
+
+            Cloudinary cloudinary = new Cloudinary(account);
+
+            //for uploading image to cloudinary server
+            var uploadParams1 = new ImageUploadParams()
+            {
+                File = new FileDescription(@TextBox1.Text),
+                Phash = true
+            };
+            var uploadResult1 = cloudinary.Upload(uploadParams1);
+
+            var uploadParams2 = new ImageUploadParams()
+            {
+                File = new FileDescription(@TextBox2.Text),
+                Phash = true
+            };
+            var uploadResult2 = cloudinary.Upload(uploadParams2);
+            
+            //retriving phash values
+            phash1 = uploadResult1.Phash;
+            phash2 = uploadResult2.Phash;
+
+            //for calculating similarity scale
+            int phash_distance = calcXor(phash1, phash2);
+            float temp = (float)phash_distance;
+            float similarity_value = (1 - (temp / 128))*100;
+            sim_scale = "similarity scale: " + similarity_value.ToString() + " / 100";
+
+            //for similarity comment
+            if (similarity_value == 100)
+            {
+                sim_comment = "exactly same image";
+            }
+            else if (similarity_value >= 90)
+            {
+                sim_comment = "almost same image";
+            }
+            else if (similarity_value >= 75)
+            {
+                sim_comment = "slightly similar image";
+            }
+            else
+            {
+                sim_comment = "two different images";
+            }
+
+        }
+        catch (Exception k)
+        {
+            Response.Write("<script>alert('error in url :make sure you entered the correct url to the image!')</script>");
+        }
+       
+       
+    }
+
+    //for comparing two phash values
+    public int calcXor(string str1,string str2)
+    {
+        //for the bitcount of no:of 1's
+        int count = 0;
+        
+            //converting str1 to byte array
+            byte[] byt_string1 = System.Text.Encoding.UTF8.GetBytes(str1);
+
+
+            //converting byte array to bits
+            BitArray bit_string1 = new BitArray(byt_string1);
+
+            //converting str2 to byte array
+            byte[] byt_string2 = System.Text.Encoding.UTF8.GetBytes(str2);
+
+
+            //converting byte array to bits
+            BitArray bit_string2 = new BitArray(byt_string2);
+
+            bit_string1.Xor(bit_string2);
+            
+       
+        for (int i = 0; i < bit_string1.Length; i++)
+        {
+        
+            
+         //   counting number of 1's in bit_string1[]
+            if (bit_string1[i].Equals(true))
+            {
+                ++count;
+            }
+        }
+        
+        return count;
+    
+        
+    }
+
+   
+
+    
+    
+}
